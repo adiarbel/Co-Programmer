55ce8f3fd39395ecb9eb4adaa1c7d5d262eb7476
adiarbel <adiarbel.a@gmail.com>
2015-12-10 14:50:15 +0200

    the Service

---
+++ DuplexService/DuplexService/EditService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace DuplexService
+{
+    [ServiceBehavior(ConcurrencyMode=ConcurrencyMode.Reentrant)]
+    public class EditService : IEditService
+    {
+        public void NormalFunction()
+        {
+            IEditServiceCallBack callback = OperationContext.Current.GetCallbackChannel<IEditServiceCallBack>();
+            callback.CallBackFunction("Got it!");
+        }
+        public void SendCaretPosition(string location)
+        {
+            IEditServiceCallBack callback = OperationContext.Current.GetCallbackChannel<IEditServiceCallBack>();
+            callback.CallBackFunction(location+" From Service");
+        }
+    }
+}
---
+++ DuplexService/DuplexService/IEditService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace DuplexService
+{
+    [ServiceContract(CallbackContract = typeof(IEditServiceCallBack))]
+    public interface IEditService
+    {
+
+        [OperationContract]
+        void SendCaretPosition(string location);
+        [OperationContract]
+        void NormalFunction();
+    }
+    public interface IEditServiceCallBack
+    {
+        [OperationContract]
+        void CallBackFunction(string str);
+    }
+}
