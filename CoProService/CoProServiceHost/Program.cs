bf4839530cb7f6056cc46789d048830688ad9316
adiarbel <adiarbel.a@gmail.com>
2016-03-18 22:31:40 +0200

    18032016

---
+++ CoProService/CoProServiceHost/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace CoProServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(CoProService.CoProService));
+            host.Open();
+            Console.WriteLine("Host started @" + DateTime.Now.ToString());
+            Console.ReadLine();
+        }
+    }
+}
