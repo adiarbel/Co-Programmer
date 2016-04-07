cb13b030f3375fe81fb978f290505861093016eb
adiarbel <adiarbel.a@gmail.com>
2016-04-07 14:32:21 +0300

    06042016

--- CoProService/CoProServiceHost/Program.cs
+++ CoProService/CoProServiceHost/Program.cs
@@ -30,6 +30,25 @@ namespace CoProServiceHost
                     host = new ServiceHost(typeof(CoProService.CoProService), new Uri[] { new Uri("http://localhost:" + port), new Uri("net.tcp://localhost:" + (port + 10)) });
                 }
             }
+            Type tNetFwPolicy2 = Type.GetTypeFromProgID("HNetCfg.FwPolicy2");
+            INetFwPolicy2 fwPolicy2 = (INetFwPolicy2)Activator.CreateInstance(tNetFwPolicy2);
+            var currentProfiles = fwPolicy2.CurrentProfileTypes;
+
+            // Let's create a new rule
+
+            INetFwRule2 inboundRule = (INetFwRule2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FWRule"));
+            inboundRule.Enabled = true;
+            inboundRule.Protocol = 6; // TCP
+            inboundRule.LocalPorts = (port+10).ToString();
+            // ...
+            inboundRule.Profiles = currentProfiles;
+            inboundRule.Name = "8090";
+            inboundRule.Description = "opens it";
+            inboundRule.Action = NET_FW_ACTION_.NET_FW_ACTION_ALLOW;
+            inboundRule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_IN;
+
+            INetFwPolicy2 firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+            firewallPolicy.Rules.Add(inboundRule);
             Console.WriteLine("Host started @" + DateTime.Now.ToString());
             Console.WriteLine(host.BaseAddresses[0].ToString() + '\n' + host.BaseAddresses[1].ToString());
             Console.ReadLine();
